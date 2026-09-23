87bb4d66b9a69fccbe9d42d0688c333852b6b038
Nguyen Khanh Son <ksondeptrai2k5@gmail.com>
2026-09-23 21:54:16 +0700

    Refactor code structure for improved readability and maintainability

--- src/fast_api_learn/database/connection.py
+++ src/fast_api_learn/database/connection.py
@@ -1,14 +1,9 @@
 import psycopg
-
-
-DATABASE_URL = (
-    "postgresql://fastapi:fastapi"
-    "@localhost:5432/fastapi_learn"
-)
+from fast_api_learn.core.config import settings
 
 
 def test_connection():
-    with psycopg.connect(DATABASE_URL) as connection:
+    with psycopg.connect(settings.database_url) as connection:
         with connection.cursor() as cursor:
             cursor.execute("SELECT 1")
             result = cursor.fetchone()
